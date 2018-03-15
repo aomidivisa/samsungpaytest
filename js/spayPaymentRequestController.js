1ccb68c0ef40a65e86b90e4ec7fb815719f8bd10
Omidi <aomidi@visa.com>
2018-03-15 15:40:25 -0700

    change inneradapter url

--- js/spayPaymentRequestController.js
+++ js/spayPaymentRequestController.js
@@ -6,7 +6,51 @@
 		sAdapterObj = 'paymentInitParams' in data ? new sAdapter(data.paymentInitParams) : null;
 	}
 
+
+	//Create PaymentRequest Object
+	var paymentRequest = {
+		checkoutPartner: "VisaCheckout",
+		requestPayload: {
+			data: {
+				// checkoutPaymentInfo: checkoutPaymentInfo,
+				// visaIntentData: visaData,
+				// paymentInitParams: visaConfigs
+			}
+		}
+	};
+	var metaProd = "https://ecomm.mpay.samsung.com/ew/v1/vco/w3c";
+	var metaStg = "https://ecomm.stg.mpay.samsung.com/ew/v1/vco/w3c";
+	var metaJsonUrl =  false ? metaProd : metaStg;
+	var methodData = [{
+		supportedMethods: [metaJsonUrl],
+		data: paymentRequest
+	}];
+	var details = {
+		total: {
+			label: "Total due",
+			amount: { currency: "USD", value: "00.00" }
+		}
+	};
+	paymentReqWallet = new PaymentRequest(methodData, details, {});
+
+
+
 	var _getWalletInfoCall = function(data){
+
+
+		//console.log("get walletInfo paymentRequest");
+	  // console.log(paymentReqWallet);
+    // paymentReqWallet.canMakePayment().then(function (result) {
+    //   console.log("the response from canMakepayment " + result);
+		// 	data.result = { isLinked: result };
+		// 	_sendMessage(data);
+    // }).catch(function (error) {
+    //   console.log("the error from canMakePayment " + error);
+		// 	data.result = { isLinked: result };
+    //   _sendMessage(data);
+    // });
+
+
 		if(sAdapterObj){
 			sAdapterObj.getWalletInfo()
 			.then(function(res) {
@@ -14,10 +58,11 @@
 				_sendMessage(data);
 			})
 			.catch(function(err) {
-				data.result = res;
+				data.result = err;
 				_sendMessage(data);
 			});
 		}
+
 	}
 
 	var _sendMessage = function(msg){
